9a213185af494debfd07217bd9144e368920811a
Prakaash <31387758+Prakaash-M@users.noreply.github.com>
2026-09-16 22:50:36 -0700

    Time: 0 ms (100.00%) | Memory: 19.4 MB (32.43%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,20 +1,11 @@
-# 1. Find the shortest string to use as our bounds
-# 2. Iterate using enumerate to keep track of the index (i) and character (char)
-# 3. Compare with every string
-# 4. Return a slice of the string
-# 5. If we finish the loop, the entire shortest string is the prefix
 class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
-        shortest_str = min(strs, key=len)
-
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        if not strs:
+            return ""
+        shortest_str = min(strs, key = len)
         for i, char in enumerate(shortest_str):
-            for j in strs:
-                if j[i] != char:
-                    return shortest_str[:i]
-        return shortest_str            
-
+            for other_str in strs:
+                if other_str[i] != char:
+                    return(shortest_str[:i])
+        return(shortest_str)            
             
-
-
-
-
